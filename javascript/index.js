ffe458466aa04e32c984fd5a61069a617defac04
priyanshiitaliya1409 <priyanshiitaliya32@gmail.com>
2023-08-31 14:26:25 +0530

    lakme

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+import Navbar from '../componets/nav.js';
+
+document.getElementById('navbar').innerHTML = Navbar()
+
+
+
+
+
+
+
+
+
+
+
+import footer from '../componets/footer.js';
+
+document.getElementById('footer').innerHTML = footer()
