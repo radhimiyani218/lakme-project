fd2a6c51a2da42305fafca017fbd97ef00d293f0
priyanshiitaliya1409 <priyanshiitaliya32@gmail.com>
2023-09-01 06:57:21 +0530

    lakme

---
+++ componets/footer.js
@@ -0,0 +1,72 @@
+const footer =()=>{
+    return `
+    <!-- footer -->
+<footer class="padding-100">
+    <div class="container">
+        <div class="row">
+            <div class="w-24">
+                <div class="f-1">
+                    <img src="/image/logo_155x_cca6b9a2-970e-4c38-a903-e6dc821dbfa5_155x.png" alt="">
+                    <div class="f-2">
+                        <p>Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, sed <br>     do
+                            et olore magna aliqua enenatis</p>
+                    </div>
+                    <p class="years">© 2015 — 2023</p>
+                </div>
+            </div>
+            <div class="w-24">
+                <div class="f-3">
+                    <h2>HELP</h2>
+                    <div class="row">
+                        <ul class="f-4">
+                            <li>FAQ</li>
+                            <li>Contact Us</li>
+                            <li>Cancel Order</li>
+                            <li>Track Order</li>
+                            <li>Terms & Conditions</li>
+                            <li>Privacy Notice</li>
+                            <li>Cookie Notice</li>
+                            <li>Accessibility</li>
+                        </ul>             
+                    </div>
+                </div>
+            </div>
+            <div class="w-24">
+                <div class="f-3">
+                    <h2>ACCOUNT</h2>
+                    <div class="row">
+                        <ul class="f-4">
+                            <li>account</li>
+                        </ul>             
+                    </div>
+                </div>
+            </div>
+            
+            <div class="w-24">
+                <div class="f-6">
+                    <h2>Contact Info</h2>
+                    <h6>
+                        7th Tea Street • Santa Monica, CA
+                    </h6>
+                <div class="row">
+                    <p>Phone: </p>
+                    <span class="text-1">987.654.3210</span>
+                </div>
+                <div class="f-7-icn">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="f-8">
+        <p class="text-center copyright">UDesign WordPress 
+            © 2023. All Rights Reserved</p>
+    </div>
+</footer>`
+}
+
+
+export default footer
